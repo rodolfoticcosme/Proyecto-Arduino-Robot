fb8b12cfa332964a0daa072e21b5af2bb2cec0fe
rodolfoticcosme <rodolfoticcosme@gmail.com>
2019-07-11 01:09:41 +0200

    Fotos, la app de app inventor y el sketch

--- Brazo_Robot_Bluetooth/Brazo_Robot_Bluetooth.ino
+++ Brazo_Robot_Bluetooth/Brazo_Robot_Bluetooth.ino
@@ -1,19 +1,5 @@
-/* Arduino Control Car V2  By: El Profe Garcia
-Descargar App: https://play.google.com/store/apps/details?id=appinventor.ai_el_profe_garcia.Arduino_Control_Car_V2&hl=es   
-
- ARDUINO   L293D(Puente H)        
- 5          10
- 6          15
- 9          7
- 10         2
- 5V         1, 9, 16
- GND        4, 5, 12, 13
- 
- El motor 1 se conecta a los pines 3 y 6 del Puente H
- El motor 2 se conecta a los pines 11 y 14 del Puente H
- 
- La fuente de alimentacion de los Motores se conecta a tierra y
- el positivo al pin 8 del puennte H. 
+/* Inspirado en El Profe Garcia
+  
  
  Conexion del Modulo Bluetooth HC-06 y el Arduino
  ARDUINO    Bluetooth HC-06 
@@ -21,167 +7,92 @@ Descargar App: https://play.google.com/store/apps/details?id=appinventor.ai_el_p
  1 (TX)       RX
  5V           VCC
  GND          GND
- !!Cuidado!! Las conexiones de TX y RX al modulo Bluetooth deben estar desconectadas
- en el momento que se realiza la carga del codigo (Sketch) al Arduino.
  
- Conexion Sensor Ultrasonido HC-SR04
- ARDUINO    Ultrasonido HC-SR04 
- 2            Echo
- 3            Trig
- 5V           VCC
- GND          Gnd
+ 
+ 
 
- Conexion Servo SG90
- ARDUINO    Ultrasonido HC-SR04 
- 11           Signal
- 5V           VCC
- GND          Gnd
  */
 
-#include <Servo.h>                // Incluye la libreria Servo
-Servo servo1;                    // Crea el objeto servo1 con las caracteristicas de Servo
+#include <Servo.h>                // Incluye la libreria Servo                  
+#include <SoftwareSerial.h> // Al incluir esta liobrería podemos usar el HC-06 con otros pines que no son el 0 y el 1
 
-int izqA = 5; 
-int izqB = 6; 
-int derA = 9; 
-int derB = 10; 
+SoftwareSerial BT1(2,3); // Crea el objeto BT1. Pines 2 y 3 para RX, TX recordar que se cruzan
+Servo servo1;   // Crea el objeto servo1 con las caracteristicas de Servo
+Servo servo2;   // Crea el objeto servo2 con las caracteristicas de Servo
+int izqA = 13;  
+int derA = 12;  
 int vel = 255;            // Velocidad de los motores (0-255)
-int estado = 'c';         // inicia detenido
+int estado=9;         // inicia detenido
 
-int pecho = 2;            // define el pin 2 como (pecho) para el Ultrasonido
-int ptrig = 3;            // define el pin 3 como (ptrig) para el Ultrasonido
+int tx = 2;            // define el pin 2 como tx del HC-06
+int rx = 3;            // define el pin 3 como rx del HC-06
 int duracion, distancia;  // para Calcular distacia
+char dato='o';
+int angServo1=90;
+int angServo2=90;
 
 void setup()  { 
   Serial.begin(9600);    // inicia el puerto serial para comunicacion con el Bluetooth
+  Serial.println("Enter AT commands:");
+  BT1.begin(9600);
   pinMode(derA, OUTPUT);
-  pinMode(derB, OUTPUT);
   pinMode(izqA, OUTPUT);
-  pinMode(izqB, OUTPUT);
   
-  pinMode(pecho, INPUT);   // define el pin 2 como entrada (pecho) 
-  pinMode(ptrig,OUTPUT);   // define el pin 3 como salida  (ptrig) 
-  pinMode(13,OUTPUT);
-
-  servo1.attach(11,660,1400);    // Asocia el servo1 al pin 11, define el min y max del ancho del pulso  
-                                 // eso depende del fabricante del servo
+  servo1.attach(6,660,1400);    // Asocia el servo1 al pin 11, define el min y max del ancho del pulso  
+  servo2.attach(9,660,1400);                              // eso depende del fabricante del servo
 } 
 
 void loop()  { 
+  int t1=100;
+  int t2=20;
+  int angulo=5;
+  dato='h';
 
-  if(Serial.available()>0){        // lee el bluetooth y almacena en estado
-    estado = Serial.read();
+if (BT1.available())
+    {  
+    dato = BT1.read();  //Lee cada carácter uno por uno y se almacena en una variable
+    Serial.println(dato);  //Imprimimos en la consola el carácter recibido
+    }
+  if (dato!='a' || dato!='b'){
+     digitalWrite(derA, LOW);     
+    digitalWrite(izqA, LOW);   
+    }        
+
+  
+  if(dato=='a'){           // Boton desplazar al Frente
+    digitalWrite(derA, HIGH);     
+    digitalWrite(izqA, LOW);       
+    delay(t1);
   }
-  if(estado=='a'){           // Boton desplazar al Frente
-    analogWrite(derB, 0);     
-    analogWrite(izqB, 0); 
-    analogWrite(derA, vel);  
-    analogWrite(izqA, vel);       
+  if(dato=='b'){          // 
+    digitalWrite(derA, LOW);     
+    digitalWrite(izqA, HIGH);      
+    delay(t1);
   }
-  if(estado=='b'){          // Boton IZQ 
-    analogWrite(derB, 0);     
-    analogWrite(izqB, 0); 
-    analogWrite(derA, 0);  
-    analogWrite(izqA, vel);      
-  }
-  if(estado=='c'){         // Boton Parar
-    analogWrite(derB, 0);     
-    analogWrite(izqB, 0); 
-    analogWrite(derA, 0);    
-    analogWrite(izqA, 0); 
-  }
-  if(estado=='d'){          // Boton DER
-    analogWrite(derB, 0);     
-    analogWrite(izqB, 0);
-    analogWrite(izqA, 0);
-    analogWrite(derA, vel);  
+  if(dato=='c'){
+     angServo1=angServo1+angulo; 
+     servo1.write(angServo1); //Mover el servo1 hacia la Izq
+     delay(t2);          //
+      }
+  if(dato=='d'){ 
+    angServo1=angServo1-angulo; 
+    servo1.write(angServo1); ////Mover el servo1 hacia la Dcha
+     delay(t2);          //
   } 
-
-  if(estado=='e'){          // Boton Reversa
-    analogWrite(derA, 0);    
-    analogWrite(izqA, 0);
-    analogWrite(derB, vel);  
-    analogWrite(izqB, vel);      
+if(dato=='e'){
+     angServo2=angServo2+angulo; 
+     servo2.write(angServo2); //Mover el servo2 hacia la Izq
+     delay(t2);          //
+      }
+  if(dato=='f'){ 
+    angServo2=angServo2-angulo; 
+    servo2.write(angServo2); ////Mover el servo2 hacia la Dcha
+     delay(t2);          //
   }
   
-  if (estado =='f'){               // Boton ON,  se mueve sensando distancia 
+     
   
-     digitalWrite(ptrig, HIGH);   // genera el pulso de trigger por 10us
-     delay(0.01);
-     digitalWrite(ptrig, LOW);
-     
-     duracion = pulseIn(pecho, HIGH);          // Lee el tiempo del Echo
-     distancia = (duracion/2) / 29;            // calcula la distancia en centimetros
-     delay(10); 
-     
-     if (distancia <= 15 && distancia >=2){    // si la distancia es menor de 15cm
-        digitalWrite(13,HIGH);                 // Enciende LED
-        
-        analogWrite(derB, 0);             // Parar los motores por 200 mili segundos
-        analogWrite(izqB, 0); 
-        analogWrite(derA, 0);    
-        analogWrite(izqA, 0); 
-        delay (200);
-        
-        analogWrite(derB, vel);          // Reversa durante 500 mili segundos
-        analogWrite(izqB, vel); 
-        delay(500);           
-        
-        analogWrite(derB, 0);            // Girar durante 600 milisegundos   
-        analogWrite(izqB, 0); 
-        analogWrite(derA, 0);  
-        analogWrite(izqA, vel);  
-        delay(600);
-        
-        digitalWrite(13,LOW);
-     }
-     else{                             // Si no hay obstaculos se desplaza al frente  
-         analogWrite(derB, 0);     
-         analogWrite(izqB, 0); 
-         analogWrite(derA, vel);  
-         analogWrite(izqA, vel); 
-     }
-  }
-  if(estado=='g'){                   // Boton SER, activa el Servomotor
-    servo1.write(30);                // Gira el servo a 30 grados  
-    delay(1000);                     // Espera 1000 mili segundos a que el servo llegue a la posicion
-
-    servo1.write(90);                // Gira el servo a 90 grados  
-    delay(700);                      // Espera 700 mili segundos a que el servo llegue a la posicion 
  
-    servo1.write(150);               //Gira el servo a 150 grados 
-    delay(700);  
-  }
 
-    if(estado=='h'){                 //Programacion de los botones 13-10-9-6-5
-    digitalWrite(13,1);        
-  }
-      if(estado=='m'){           
-    digitalWrite(13,0);        
-  }
-    if(estado=='i'){           
-    digitalWrite(derB,1);        
-  }
-    if(estado=='n'){           
-    digitalWrite(derB,0);        
-  }
-    if(estado=='j'){           
-    digitalWrite(derA,1);        
-  }
-    if(estado=='o'){           
-    digitalWrite(derA,0);        
-  }
-    if(estado=='k'){           
-    digitalWrite(izqB,1);        
-  }
-    if(estado=='p'){           
-    digitalWrite(izqB,0);        
-  }
-    if(estado=='l'){           
-    digitalWrite(izqA,1);        
-  }
-    if(estado=='q'){           
-    digitalWrite(izqA,0);        
-  }
   
 }
